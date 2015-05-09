5736ae6a71a5630fed6c7273ae50946f28115a5f
isac322 <isac322@naver.com>
2015-05-09 18:41:29 +0900

    Init
    Init

---
+++ src/Token.java
@@ -0,0 +1,121 @@
+/**
+ * Created by º´ÈÆ on 2015-05-09.
+ */
+public class Token {
+	private static final int KEYWORDS = TokenType.Eof.ordinal();
+
+	private static final String[] reserved = new String[KEYWORDS];
+	private static Token[] token = new Token[KEYWORDS];
+
+	public static final Token eofTok = new Token(TokenType.Eof, "<<EOF>>");
+	public static final Token voidTok = new Token(TokenType.Void, "void");
+	public static final Token boolTok = new Token(TokenType.Bool, "bool");
+	public static final Token intTok = new Token(TokenType.Int, "int");
+	public static final Token charTok = new Token(TokenType.Char, "char");
+	public static final Token floatTok = new Token(TokenType.Float, "float");
+	public static final Token dateTok = new Token(TokenType.Date, "date");
+	public static final Token timeTok = new Token(TokenType.Time, "time");
+	public static final Token mainTok = new Token(TokenType.Main, "main");
+	public static final Token trueTok = new Token(TokenType.True, "true");
+	public static final Token falseTok = new Token(TokenType.False, "false");
+	public static final Token ifTok = new Token(TokenType.If, "if");
+	public static final Token elseTok = new Token(TokenType.Else, "else");
+	public static final Token elseIfTok = new Token(TokenType.ElseIf, "else if");
+	public static final Token whileTok = new Token(TokenType.While, "while");
+	public static final Token forTok = new Token(TokenType.For, "for");
+	public static final Token switchTok = new Token(TokenType.Switch, "switch");
+	public static final Token caseTok = new Token(TokenType.Case, "case");
+	public static final Token leftBraceTok = new Token(TokenType.LeftBrace, "{");
+	public static final Token rightBraceTok = new Token(TokenType.RightBrace, "}");
+	public static final Token leftBracketTok = new Token(TokenType.LeftBracket, "[");
+	public static final Token rightBracketTok = new Token(TokenType.RightBracket, "]");
+	public static final Token leftParenTok = new Token(TokenType.LeftParen, "(");
+	public static final Token rightParenTok = new Token(TokenType.RightParen, ")");
+	public static final Token semicolonTok = new Token(TokenType.Semicolon, ";");
+	public static final Token commaTok = new Token(TokenType.Comma, ",");
+	public static final Token assignTok = new Token(TokenType.Assign, "=");
+	public static final Token eqeqTok = new Token(TokenType.Equals, "==");
+	public static final Token ltTok = new Token(TokenType.Less, "<");
+	public static final Token lteqTok = new Token(TokenType.LessEqual, "<=");
+	public static final Token gtTok = new Token(TokenType.Greater, ">");
+	public static final Token gteqTok = new Token(TokenType.GreaterEqual, ">=");
+	public static final Token notTok = new Token(TokenType.Not, "!");
+	public static final Token noteqTok = new Token(TokenType.NotEqual, "!=");
+	public static final Token plusTok = new Token(TokenType.Plus, "+");
+	public static final Token plusAssignTok = new Token(TokenType.PlusAssign, "+=");
+	public static final Token plusPlusTok = new Token(TokenType.PlusPlus, "++");
+	public static final Token minusTok = new Token(TokenType.Minus, "-");
+	public static final Token minusAssignTok = new Token(TokenType.MinusAssign, "-=");
+	public static final Token minusMinusTok = new Token(TokenType.MinusMinus, "--");
+	public static final Token multiplyTok = new Token(TokenType.Multiply, "*");
+	public static final Token multiplyAsssignTok = new Token(TokenType.MultiplyAssign, "*=");
+	public static final Token divideTok = new Token(TokenType.Divide, "/");
+	public static final Token divideAssignTok = new Token(TokenType.DivideAssign, "/=");
+	public static final Token modTok = new Token(TokenType.Mod, "%");
+	public static final Token modAssignTok = new Token(TokenType.ModAssign, "%=");
+	public static final Token andTok = new Token(TokenType.And, "&&");
+	public static final Token orTok = new Token(TokenType.Or, "||");
+
+	private TokenType type;
+	private String value = "";
+
+	private Token(TokenType t, String v) {
+		type = t;
+		value = v;
+		if (t.compareTo(TokenType.Eof) < 0) {
+			int ti = t.ordinal();
+			reserved[ti] = v;
+			token[ti] = this;
+		}
+	}
+
+	public TokenType type() {
+		return type;
+	}
+
+	public String value() {
+		return value;
+	}
+
+	public static Token keyword(String name) {
+		char ch = name.charAt(0);
+		if (ch >= 'A' && ch <= 'Z') return mkIdentTok(name);
+		for (int i = 0; i < KEYWORDS; i++)
+			if (name.equals(reserved[i])) return token[i];
+		return mkIdentTok(name);
+	} // keyword
+
+	public static Token mkIdentTok(String name) {
+		return new Token(TokenType.Identifier, name);
+	}
+
+	public static Token mkIntLiteral(String name) {
+		return new Token(TokenType.IntLiteral, name);
+	}
+
+	public static Token mkFloatLiteral(String name) {
+		return new Token(TokenType.FloatLiteral, name);
+	}
+
+	public static Token mkCharLiteral(String name) {
+		return new Token(TokenType.CharLiteral, name);
+	}
+
+	public static Token mkTimeLiteral(String name) {
+		return new Token(TokenType.TimeLiteral, name);
+	}
+
+	public static Token mkDateLiteral(String name) {
+		return new Token(TokenType.DateLiteral, name);
+	}
+
+	public String toString() {
+		if (type.compareTo(TokenType.Identifier) < 0) return value;
+		return type + "\t" + value;
+	} // toString
+
+	public static void main(String[] args) {
+		System.out.println(eofTok);
+		System.out.println(whileTok);
+	}
+}
---
+++ src/TokenType.java
@@ -0,0 +1,11 @@
+/**
+ * Created by º´ÈÆ on 2015-05-06.
+ */
+public enum TokenType {
+	Void, Bool, Int, Char, Float, Date, Time, Main, True, False, If, Else, ElseIf, While, For, Switch, Case, Eof,
+	LeftBrace, RightBrace, LeftBracket, RightBracket, LeftParen, RightParen,
+	Semicolon, Comma, Assign, Equals, Less, LessEqual, Greater, GreaterEqual, Not, NotEqual,
+	Plus, PlusAssign, PlusPlus, Minus, MinusAssign, MinusMinus, Multiply, MultiplyAssign,
+	Divide, DivideAssign, Mod, ModAssign, And, Or,
+	Identifier, IntLiteral, FloatLiteral, CharLiteral, TimeLiteral, DateLiteral
+}
