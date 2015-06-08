ce4f0a5a93a14f86d1f63fe2df4fd2f94a7e8f6e
isac322 <isac322@naver.com>
2015-06-08 15:24:28 +0900

    fix test cods

--- test_Code/code_generator_test-9.cpp
+++ test_Code/code_generator_test-9.cpp
@@ -1,5 +1,19 @@
 int main() {
-	date t = 2014.4.30;
+	time t = 0:0:0;
+
+	t += 0:1:0;
 
 	write(t);
+	lf();
+
+	t *= 10;
+	write(t);
+	lf();
+
+	t *= 10;
+	write(t);
+	lf();
+
+	write(1:0:0 * 24  - 0:0:1);
+	lf();
 }
